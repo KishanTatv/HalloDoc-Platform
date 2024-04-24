390a2244aace720c61756415e3d00b187b5dc3d0
Kishan Bhadani <kishan.bhadani@etatvasoft.com>
2024-04-24 18:10:32 +0530

    Task Added

---
+++ Assignment/AssignmentTask.Entity/Models/Borrower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssignmentTask.Entity.Models
+{
+    [Table("borrower")]
+    public partial class Borrower
+    {
+        public Borrower()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("city")]
+        [StringLength(256)]
+        public string City { get; set; } = null!;
+
+        [InverseProperty("Borrower")]
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ Assignment/AssignmentTask/Controllers/LibraryController.cs
@@ -0,0 +1,89 @@
+﻿using AssignmentTask.Entity.Models;
+using AssignmentTask.Entity.ViewModels;
+using AssignmentTask.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentTask.Controllers
+{
+    public class LibraryController : Controller
+    {
+
+        private readonly ILibrary _library;
+
+        public LibraryController(ILibrary library)
+        {
+            _library = library;
+        }
+
+        #region Main Page
+        public IActionResult LibraryHome()
+        {
+            return View();
+        }
+
+        public IActionResult LibraryData(string searchName, int page)
+        {
+            int pageSize = 5;
+            HomeDataTableModel booksDetail = _library.getBooks(searchName, page, pageSize);
+            ViewBag.CurrentPage = page;
+            ViewBag.TPage = Math.Ceiling(booksDetail.totalRecord.Value / 5.0);
+            ViewBag.TotalRecord = booksDetail.totalRecord.Value;
+            return PartialView("_libTable", booksDetail);
+        }
+        #endregion
+
+
+        #region Popup
+        public IActionResult AddBook(int id)
+        {
+            if (id != 0)
+            {
+                BookPopupViewModel book = _library.getSingleBook(id);
+                return PartialView("_PopupAddBook", book);
+            }
+            return PartialView("_PopupAddBook");
+        }
+        #endregion
+
+
+        #region Add
+        public IActionResult BookDetailSave(BookPopupViewModel modelData)
+        {
+            if (ModelState.IsValid)
+            {
+                _library.addBookDetail(modelData);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+
+
+        #region Update
+        public IActionResult UpdateBookDetail(BookPopupViewModel modelData)
+        {
+            if (ModelState.IsValid)
+            {
+                _library.updateBookDetail(modelData);
+                return Ok();
+            }
+            return BadRequest();
+        }
+        #endregion
+
+
+        #region Delete
+        public IActionResult BookDetailDelete(int id)
+        {
+            if (id != 0)
+            {
+                _library.removeBookDetail(id);
+            }
+            return Ok();
+        }
+        #endregion
+    }
+}
